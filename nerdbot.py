0ce942ae7423c298cc6affd4bab63fa7f8c6cccf
krut101 <htaedshararam@gmail.com>
2017-12-10 13:24:36 +0300

    Add files via upload

---
+++ nerdbot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix = '')
+
+@bot.event
+async def on_ready():
+    print("Let's go! I'm ready!")
+@bot.command(pass_context = True)
+async def L(ctx):
+    await bot.say("In process of handing the L to cookieXL...")
+    await bot.say("Done!")
+@bot.command(pass_context = True)
+async def think(ctx):
+    await bot.say(":thinking:")
+@bot.command(pass_context = True)
+async def bape(ctx):
+    await bot.say("BAPE FTW")
+bot.run("Mzg5MTIzNDI1NTcyMDkzOTUz.DQ3E_A.XEbs_eriBnSOvin2_EXEwty79Kk")
